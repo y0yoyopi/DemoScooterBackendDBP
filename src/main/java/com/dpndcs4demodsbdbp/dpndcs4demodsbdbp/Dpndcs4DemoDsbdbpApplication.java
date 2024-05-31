6b44a9220143e22c7ba934bc60426d878525c219
Y0yoyopi <you@example.com>
2024-05-31 06:06:58 -0500

    Initial commit

---
+++ src/main/java/com/dpndcs4demodsbdbp/dpndcs4demodsbdbp/Dpndcs4DemoDsbdbpApplication.java
@@ -0,0 +1,13 @@
+package com.dpndcs4demodsbdbp.dpndcs4demodsbdbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dpndcs4DemoDsbdbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dpndcs4DemoDsbdbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dpndcs4demodsbdbp/dpndcs4demodsbdbp/Dpndcs4DemoDsbdbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpndcs4demodsbdbp.dpndcs4demodsbdbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dpndcs4DemoDsbdbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
